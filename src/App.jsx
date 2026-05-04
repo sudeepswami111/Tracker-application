e855f7f97ae7f418fdeeb7a9d18f3ea431226c21
sudeep swami <sudeepswami111@gmail.com>
2026-05-04 09:43:27 +0530

    Fix React Router blank screen using HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar.jsx';
 import Header from './components/Header.jsx';
 import MobileNav from './components/MobileNav.jsx';
